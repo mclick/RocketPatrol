86798fb6287775ad66357b052da5f8f8e5fd3c53
mclick <mclick@ucsc.edu>
2021-06-25 15:38:27 -0700

    sound effects added

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -7,6 +7,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
       scene.add.existing(this);
       this.isFiring = false;
       this.moveSpeed = 2;
+      this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     
     }
     update(){
@@ -18,9 +19,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
                 this.x+=this.moveSpeed;
             }
         }
-        if(Phaser.Input.Keyboard.JustDown(keyF)){
+        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
-        }
+            this.sfxRocket.play();  // play sfx
+          }
         if(this.isFiring &&this.y>=borderUISize * 3 + borderPadding){
             this.y -=this.moveSpeed;
         }
--- src/scenes/play.js
+++ src/scenes/play.js
@@ -8,6 +8,10 @@ class Play extends Phaser.Scene {
         this.load.image('spaceship', './assets/spaceship.png');
         this.load.image('starfield', './assets/starfield.png');
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+        // load audio
+        this.load.audio('sfx_select', './assets/blip_select12.wav');
+        this.load.audio('sfx_explosion', './assets/explosion38.wav');
+        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
       }
     create(){
         // place tile sprite
@@ -36,33 +40,65 @@ class Play extends Phaser.Scene {
           frames: this.anims.generateFrameNumbers('explosion', {start: 0, end: 9, first: 0}),
           frameRate: 30
         })
+        this.p1Score = 0;
+          // display score
+        let scoreConfig = {
+          fontFamily: 'Courier',
+          fontSize: '28px',
+          backgroundColor: '#F3B141',
+          color: '#843605',
+          align: 'right',
+          padding: {
+            top: 5,
+            bottom: 5,
+          },
+          fixedWidth: 100
+        }
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
+        // GAME OVER flag
+        this.gameOver = false;
+        // 60-second play clock
+        scoreConfig.fixedWidth = 0;
+        //I added this line, so i can set the clock to any number of seconds easily
+        let sec = 60
+        this.clock = this.time.delayedCall(sec*1000, () => {
+          this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+           this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+           this.gameOver = true;
+        }, null, this);
     }
     update() {
-        this.starfield.tilePositionX -= 4;
+      // check key input for restart
+      if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+        this.scene.restart();
+      } 
+      this.starfield.tilePositionX -= 4;
+      if(!this.gameOver){
         this.p1Rocket.update();
         this.ship01.update();
         this.ship02.update();
         this.ship03.update();
-        if(this.checkCollision(this.p1Rocket, this.ship03)) {
-          this.p1Rocket.reset();
-          this.shipExplode(this.ship03);
-        }
-        if (this.checkCollision(this.p1Rocket, this.ship02)) {
-          this.p1Rocket.reset();
-          this.shipExplode(this.ship02);
-        }
-        if (this.checkCollision(this.p1Rocket, this.ship01)) {
-          this.p1Rocket.reset();
-          this.shipExplode(this.ship01);
-        }
       }
+      if(this.checkCollision(this.p1Rocket, this.ship03)) {
+        this.p1Rocket.reset();
+        this.shipExplode(this.ship03);
+      }
+      if (this.checkCollision(this.p1Rocket, this.ship02)) {
+        this.p1Rocket.reset();
+        this.shipExplode(this.ship02);
+      }
+      if (this.checkCollision(this.p1Rocket, this.ship01)) {
+        this.p1Rocket.reset();
+        this.shipExplode(this.ship01);
+      }
+    }
 
     checkCollision(rocket,ship){
       if (rocket.x < ship.x + ship.width && 
         rocket.x + rocket.width > ship.x && 
         rocket.y < ship.y + ship.height &&
         rocket.height + rocket.y > ship. y) {
-            return true;
+          return true;
       } else {
         return false;
       }
@@ -77,6 +113,9 @@ class Play extends Phaser.Scene {
         ship.reset();                         // reset ship position
         ship.alpha = 1;                       // make ship visible again
         boom.destroy();                       // remove explosion sprite
-      });       
+      });    
+      this.p1Score += ship.points;
+      this.scoreLeft.text = this.p1Score;   
+      this.sound.play('sfx_explosion');      
     }
 }
